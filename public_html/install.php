da1669874a10d034edf0fbd81802381feac218c3
Aistė <71317858+aisteRutkauskaite@users.noreply.github.com>
2020-12-01 16:06:27 +0200

    Initial commit

---
+++ public_html/install.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\App;
+
+require '../bootloader.php';
+
+App::$db = new FileDB(DB_FILE);
+
+App::$db->createTable('users');
+App::$db->insertRow('users', ['email' => 'test@test.lt', 'password' => 'test']);
+
+App::$db->createTable('items');
