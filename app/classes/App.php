8af1b9fad3a08481cd146fcbfcfc4d208ebdfe0c
Aistė <71317858+aisteRutkauskaite@users.noreply.github.com>
2020-12-09 16:08:56 +0200

    Atnaujinti failai 2020-12-09 nebėra login.php, register.php. install.php, logout.php nauji controller failai

--- app/classes/App.php
+++ app/classes/App.php
@@ -2,19 +2,31 @@
 
 namespace App;
 
+
 use Core\FileDB;
+use Core\Router;
 use Core\Session;
+
 
 class App
 {
-    public static $db;
-    public static $session;
+    public static FileDB $db;
+    public static Session $session;
+    public static Router $router;
 
     public function __construct()
     {
         self::$db = new FileDB(DB_FILE);
         self::$db->load();
+
         self::$session = new Session();
+        self::$router = new Router();
+
+    }
+
+    public function run()
+    {
+        print self::$router::run();
     }
 
     public function __destruct()
--- bootloader.php
+++ bootloader.php
@@ -1,19 +1,22 @@
 <?php
 
-use App\App;
+
+//use App\App;
+//use Core\Cookies;
 
 define('ROOT', __DIR__);
 define('DB_FILE', ROOT . '/app/data/db.json');
 // App
 require 'app/functions/form/validators.php';
-require 'app/functions/generators.php';
+require 'app/classes/App.php';
 // Core
 require 'core/functions/file.php';
 require 'core/functions/html.php';
 require 'core/functions/form/core.php';
 require 'core/functions/form/validators.php';
 
+//Composer
 require 'vendor/autoload.php';
-
-$app = new App();
+require 'app/Config/Routes.php';
+//$app = new App();
 
